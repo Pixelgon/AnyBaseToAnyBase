6f34f0cbf770a738968eca269cfc35ba00fe6863
Pixelgon <pixelgon4@gmail.com>
2022-02-17 13:56:32 +0100

    Almost complete

--- main.py
+++ main.py
@@ -1,34 +1,52 @@
 # Vytvoril Matej Matejka
 # Prevod z jakekoliv soustavy do destikove
 
-
+def split(word):
+    return list(word)
 zaklad = int(input("Zadejte číselnou soustavu ve které je vaše číslo: "))
+# Overime zda jsme zadali spravnou soustavu
 while zaklad < 2 or zaklad > 36:
         print("Zadali jste špatnou soustavu")
-        zaklad = int(input("Zadejte prosím soustavu znovu: "))
-
+        zaklad = int(input("Zadejte soustavu znovu: "))
 cislo_vstup = str(input("Zadejte číslo, které chcete převést: "))
 cislo_vstup = cislo_vstup.strip()
-
-def split(word):
-        return list(word)
-
 cislo_vstup = split(cislo_vstup)
-
-# TODO
-#provedeme kontrolu pismen ve cislo_vstupni zda jsou mensi nez zaklad_vstup
-
-# =A * 16^4 + B * 16^3 + 1 * 16^2 + 2 * 16^1 + F * 16^0
-
-# = cislice_v_danem_radu * zaklad ** pozice_dane_cislice
+# Pismena nahradime cisli
+for a, n in enumerate(cislo_vstup):
+    if n == 'A':
+        cislo_vstup[a] = '10'
+for b, n in enumerate(cislo_vstup):
+    if n == 'B':
+        cislo_vstup[b] = '11'
+for c, n in enumerate(cislo_vstup):
+    if n == 'C':
+        cislo_vstup[c] = '12'
+for d, n in enumerate(cislo_vstup):
+    if n == 'D':
+        cislo_vstup[d] = '13'
+for e, n in enumerate(cislo_vstup):
+    if n == 'E':
+        cislo_vstup[e] = '14'
+for f, n in enumerate(cislo_vstup):
+    if n == 'F':
+        cislo_vstup[f] = '15'
+cislo_vstup_map = map(int, cislo_vstup)
+cislo_vstup = list(cislo_vstup_map)
+while max(cislo_vstup) > zaklad:
+    print("Číslo se neshoduje se soustavou")
+    cislo_vstup = str(input("Zadejte číslo znovu: "))
+    cislo_vstup = cislo_vstup.strip()
+    cislo_vstup = split(cislo_vstup)
+    cislo_vstup_map = map(int, cislo_vstup)
+    cislo_vstup = list(cislo_vstup_map)
 
 cislo = 0
 mocnina = len(cislo_vstup) - 1
-
+cislice = 0
 for i in range(0, len(cislo_vstup)):
-        if cislo_vstup[i] >= '0' <= '9':
+        if cislo_vstup[i] >= 0 <= 9:
                 cislice = int(cislo_vstup[i])
-        elif cislo_vstup[i] >= 'A' <= 'Z':
+        elif cislo_vstup[i] >= "A" <= "Z":
                 cislice = ord(cislo_vstup[i]) - 55
         else:
                 print("Chyba ve vstupním čísle")
@@ -54,4 +72,3 @@ while cislo > 0:
 vysledek = vysledek[::-1]
 # Vysledek vypiseme
 print(vysledek)
-exit()
